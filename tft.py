b7118aa10f590f710e9210549165e216fead3569
Cody_Bagwandeen <codybagwandeen1@gmail.com>
2021-09-27 11:12:28 -0400

    make surrender function and startQueue function, surrender works, startQueue needs a image of the queue button

--- tft.py
+++ tft.py
@@ -1,18 +1,30 @@
 import pyautogui
 import time
 
+def surrender():
+    pyautogui.press('enter')
+    pyautogui.write("/ff", .25)
+    pyautogui.press('enter')
+
+def startQueue():
+    QueueX, QueueY = pyautogui.locateCenterOnScreen('')
+    pyautogui.moveTo(QueueX,QueueY)
+    pyautogui.click()
+
 
 temp = True
 while temp:
     currentMouseX, currentMouseY = pyautogui.position()
+    print(pyautogui.size())
     time.sleep(1)
     print(currentMouseX)
     print(currentMouseY)
 
     time.sleep(3)
-    pyautogui.write("/ff", .25)
-    pyautogui.press('enter')
+    surrender()
+    
 
     temp = False
 
     #code words, praise baal
+
