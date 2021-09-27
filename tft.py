696e14a75e06af8b5c186bb7737d33fb4ef6ba52
Cody_Bagwandeen <codybagwandeen1@gmail.com>
2021-09-27 10:51:13 -0400

    tested ability to type /ff, it works

--- tft.py
+++ tft.py
@@ -2,11 +2,17 @@ import pyautogui
 import time
 
 
-
-while True:
+temp = True
+while temp:
     currentMouseX, currentMouseY = pyautogui.position()
     time.sleep(1)
     print(currentMouseX)
     print(currentMouseY)
+
+    time.sleep(3)
+    pyautogui.write("/ff", .25)
+    pyautogui.press('enter')
+
+    temp = False
 
     #code words, praise baal
