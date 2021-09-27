11d78089f760d2b675c76057ff0545fce315a244
Cody_Bagwandeen <codybagwandeen1@gmail.com>
2021-09-27 13:47:08 -0400

    reworked accept Queue to just click until it determines it is in game, now to make surrender work

--- tft.py
+++ tft.py
@@ -1,24 +1,70 @@
+from os import putenv
 import pyautogui
 import time
 
+
 def surrender():
+    print('about to type /ff')
     pyautogui.press('enter')
-    pyautogui.write("/ff", .25)
+    pyautogui.press('/')
+    pyautogui.press('f')
+    pyautogui.press('f')
     pyautogui.press('enter')
+    print('typed /ff')
+
+    print('about to press q 6 times')
+    for index in range(6):
+        pyautogui.press('q')
+        time.sleep(0.25)
+    print('finished pressing')
+
 
 def startQueue():
+    time.sleep(3)
     QueueX, QueueY = pyautogui.locateCenterOnScreen('find_match.png')
-    pyautogui.moveTo(QueueX,QueueY)
+    pyautogui.moveTo(QueueX, QueueY)
     pyautogui.click()
+
+
+def acceptQueue():
+    inQueue = True
+    pyautogui.moveTo(984, 776)
+    while inQueue:
+        time.sleep(4)
+        pyautogui.click()
+        print('just clicked')
+        if(pyautogui.locateCenterOnScreen('in_game_checker.png') != None):
+            print('now in game')
+            inQueue = False    
+        
+
+def InGame():
+    print('meow')
+
+
+def reQueue():
+    reQueueX, reQueueY = pyautogui.locateCenterOnScreen('play_again.png')
+    pyautogui.moveTo(reQueueX,reQueueY)
+    pyautogui.click()
+    time.sleep(5)
+    
+
+
 
 
 temp = True
 while temp:
-    time.sleep(5)
+    time.sleep(2)
+
     startQueue()
+    acceptQueue()
+    #reQueue()
+    #startQueue()
+    
+
     
 
     temp = False
 
-    #code words, praise baal
+    # code words, praise baal
 
